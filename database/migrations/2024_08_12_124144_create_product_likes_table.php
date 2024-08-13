4da98e550c6b0b6989e285c7a381155befa693c4
codewithdann0 <codewithdann028@gmail.com>
2024-08-13 06:36:57 -0700

    products-like table migrated

---
+++ database/migrations/2024_08_12_124144_create_product_likes_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductLikesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('product_likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('product_likes');
+    }
+}
